d392b7499fe84d117f01742698406838ae23fbee
JohnsonZZ <465629989@qq.com>
2016-05-22 10:48:12 +0800

    domain

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -11,4 +11,11 @@ return array(
 	'DB_PREFIX' => 'zz_', // 数据库表前缀 
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
+	'APP_SUB_DOMAIN_DEPLOY'=>true, // 开启子域名配置
+	/*子域名配置 
+		*格式如: '子域名'=>array('分组名/[模块名]','var1=a&var2=b'); 
+    */ 
+	'APP_SUB_DOMAIN_RULES'=>array(   
+		'm'=>array('Admin/'),  // admin域名指向Admin分组
+	),
 );
